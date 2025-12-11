aafb9a2adca7a4a95da1ec7758e60e6108c2b3f0
bensoussang <145578837+leogeek69@users.noreply.github.com>
2025-12-11 16:38:37 +0100

    ajout de blueprint 'genre,film,'

---
+++ database/migrations/2025_12_11_150153_film.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->id();
+            $table->string('nomFil');
+            $table->Time('datFil');
+            $table->string('afiFil');
+            $table->string('desFil');
+            $table->string('typeFil');
+            $table->boolean('malVoyEnt');
+            $table->string('banAnn');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Scheme::dropIfExists('films');
+    }
+};
