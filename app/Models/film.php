76a69e8652ee0b917aae60dba10f67092da211b4
bensoussang <145578837+leogeek69@users.noreply.github.com>
2026-02-22 17:08:18 +0100

    test

--- app/Models/film.php
+++ app/Models/film.php
@@ -4,22 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class film extends Model
+class Film extends Model
 {
-    protected $table = 'film';// dis la table sur laquel il faut travailler
+    protected $table = 'film';
+    protected $primaryKey = 'idFil';
 
-    protected $primaryKey = 'idFil';//la clé primaire
-
-    public $timestamps = false;//si il y a un timestamp ou non parmis nos champs (pas le cas pour cette tbale)
-
-    protected $fillable = [
-        'nomFil',
-        'datFil',
-        'afiFil',
-        'desFil',
-        'typeFil',
-        'malVoyEnt',
-        'banAnn'
-        // le nom de tout nos champs pour dire qu'il sont remplissable par un fomrulaire (eite les fille mass assignement)
-    ];
+    public $timestamps = false;
+    protected $guarded = [];
 }
