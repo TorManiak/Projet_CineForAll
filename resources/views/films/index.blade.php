2aa8ced6dbf994f10bf402df15e8b8bb0b376802
axel.brqt <134606609+Shyr0v@users.noreply.github.com>
2026-02-26 13:42:29 +0100

    Delete resources/views/films directory

--- resources/views/films/index.blade.php
+++
@@ -1,9 +0,0 @@
-<h1>Catalogue des films</h1>
-
-@foreach($films as $film)
-    <div>
-        <h2>{{ $film->nomFil }}</h2>
-        <p>{{ $film->desFil }}</p>
-        <a href="{{ route('films.show', $film->idFil) }}">Voir détail</a>
-    </div>
-@endforeach
