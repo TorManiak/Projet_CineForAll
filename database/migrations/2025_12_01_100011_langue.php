03cdfccf33e3c8f4dda3c4bb08238d152093c3b5
bensoussang <145578837+leogeek69@users.noreply.github.com>
2026-02-26 08:25:06 +0100

    Ajout des Langue + année de sortie du film

---
+++ database/migrations/2025_12_01_100011_langue.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('langue', function(Blueprint $table) {
+            $table->id('idLan');
+            $table->string('langue');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('langue');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             PersonnaliteSeeder::class,
             NoteSeeder::class,
             JouerSeeder::class,
+            LangueSeeder::class,
         ]);
     }
 }
