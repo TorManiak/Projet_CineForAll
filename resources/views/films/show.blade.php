4a3e9173ae0ec0ab28a3cb035ce360e5abdddf2c
axel.brqt <134606609+Shyr0v@users.noreply.github.com>
2026-02-26 13:42:45 +0100

    Delete resources/views/films directory

--- resources/views/films/show.blade.php
+++
@@ -1,9 +0,0 @@
-<h1>{{ $film->nomFil }}</h1>
-
-<p>{{ $film->desFil }}</p>
-
-<p>Durée : {{ $film->datFil }}</p>
-
-<p>Type : {{ $film->typeFil }}</p>
-
-<a href="/catalogue">Retour au catalogue</a>
